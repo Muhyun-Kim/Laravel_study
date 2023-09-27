aaee62d531db13dd51636152ab6af42b5c6cb2b4
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-09-28 03:10:46 +0900

    投稿機能

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\PostsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function() {
-    return view('about');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/posts', function() {
-    return '記事リスト';
-});
+require __DIR__.'/auth.php';
 
-Route::get('/posts/{id}', [PostsController::class, 'show']);
+Route::get('post/create', [PostController::class, 'create']);
+
+Route::post('post', [PostController::class, 'store'])->name('post.store');
